b1bf7b2e9981467eccfc522d7ef2b70d988db49c
Brice Beauvillain <brice.beauvillain@bbc.co.uk>
2021-11-10 01:28:42 +0000

    feat: remove property name setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-if [ -z "$GSAAS" ]; then
-  echo "GSAAS property name not set. Quitting."
-  exit 1
-fi
-
 echo "Change directory to Source"
 cd $SOURCE_DIR
 
 echo "Get the properties"
-scene_version=npm pkg get $GSAAS.version
-scene_name=npm pkg get $GSAAS.name
+scene_version=npm pkg get gsaas.version
+scene_name=npm pkg get gsaas.name
 
 echo "return the property"
 echo "::set-output name=scene_version::$scene_version"
