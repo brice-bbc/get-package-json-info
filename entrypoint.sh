608b31c061a64f84280160c1528de14ba7b65fc2
Brice Beauvillain <52964967+brice-bbc@users.noreply.github.com>
2021-11-10 17:41:28 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,5 @@ scene_name=`npm pkg get gsaas.template_name | sed 's/^.//;s/.$//'`
 echo "GSAAS Scene name: ${scene_name}"
 
 echo "return the property"
-echo "::set-output name=scene_version::${scene_version:1:-1}"
-echo "::set-output name=scene_name::${scene_name:1:-1}"
+echo "::set-output name=scene_version::${scene_version}"
+echo "::set-output name=scene_name::${scene_name}"
