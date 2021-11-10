04c040499d2b0b23f053ff337b403b42108ef177
Brice Beauvillain <52964967+brice-bbc@users.noreply.github.com>
2021-11-10 16:20:44 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ cd $GITHUB_WORKSPACE
 
 echo "Get the properties"
 scene_version=`npm pkg get gsaas.version`
-echo "GSAAS Scene version: ${scene_version:1:-1}"
+echo "GSAAS Scene version: ${scene_version}"
 scene_name=`npm pkg get gsaas.template_name`
-echo "GSAAS Scene name: ${scene_name:1:-1}"
+echo "GSAAS Scene name: ${scene_name}"
 
 echo "return the property"
 echo "::set-output name=scene_version::${scene_version:1:-1}"
