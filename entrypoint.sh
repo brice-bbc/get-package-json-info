974ca4bf9d713787826457e0d58423e2b92760f5
Brice Beauvillain <brice.beauvillain@bbc.co.uk>
2021-11-10 12:36:35 +0000

    fix: remove double quotes from gsaas version and name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ echo "Change directory to Source"
 cd $SOURCE_DIR
 
 echo "Get the properties"
-scene_version=npm pkg get gsaas.version
-scene_name=npm pkg get gsaas.name
+scene_version=`npm pkg get gsaas.version`
+scene_name=`npm pkg get gsaas.name`
 
 echo "return the property"
-echo "::set-output name=scene_version::$scene_version"
-echo "::set-output name=scene_name::$scene_name"
+echo "::set-output name=scene_version::${scene_version:1:-1}"
+echo "::set-output name=scene_name::${scene_name:1:-1}"
